3201bbf9a79818a5208d91e48266563bb205d2bd
Andre Mauricio Zelak <andre.zelak@telefonica.com>
2017-10-26 15:28:18 -0200

    [LG] Command map
    Signed-off-by: Andre Mauricio Zelak <andre.zelak@telefonica.com>

---
+++ lg.py
@@ -0,0 +1,50 @@
+# LG command map
+
+cmds = {
+    'Power On'                  : 'ka 01 01',
+    'Power Off'                 : 'ka 01 00',
+    'Energy Saving Off'         : 'jq 01 00',
+    'Energy Saving Minimum'     : 'jq 01 01',
+    'Energy Saving Medium'      : 'jq 01 02',
+    'Energy Saving Maximum'     : 'jq 01 03',
+    'Energy Saving Auto'        : 'jq 01 04',
+    'Energy Saving Screen Off'  : 'jq 01 05',
+    'Input DTV Antenna'         : 'xb 01 00',
+    'Input DTV Cable'           : 'xb 01 01',
+    'Input Analog Antenna'      : 'xb 01 10',
+    'Input AV1'                 : 'xb 01 20',
+    'Input AV2'                 : 'xb 01 21',
+    'Input Component1'          : 'xb 01 40',
+    'Input Component2'          : 'xb 01 41',
+    'Input PC'                  : 'xb 01 60',
+    'Input HDMI1'               : 'xb 01 90',
+    'Input HDMI2'               : 'xb 01 91',
+    'Input HDMI3'               : 'xb 01 92',
+    'Input HDMI4'               : 'xb 01 93',
+    'Aspect Ratio 4:3'          : 'kc 01 01',
+    'Aspect Ratio 16:9'         : 'kc 01 02',
+    'Aspect Ratio Zoom'         : 'kc 01 04',
+    'Aspect Ratio By Program'   : 'kc 01 06',
+    'Aspect Ratio Just Scan'    : 'kc 01 09',
+    'Aspect Ratio Cinema Zoom'  : 'kc 01 10',
+    'Screen Mute Off'           : 'kd 01 00',
+    'Screen Mute On'            : 'kd 01 01',
+    'Video Out Mute On'         : 'kd 01 10',
+    'Volume Mute On'            : 'ke 01 00',
+    'Volume Mute Off'           : 'ke 01 01',
+    'Volume Level'              : 'kf 01 00', # 0 - 100
+    'Volume Up'                 : 'mc 01 02',
+    'Volume Down'               : 'mc 01 03',
+    'Picture Contrast'          : 'kg 01 00', # 0 - 100
+    'Picture Brightness'        : 'kh 01 00', # 0 - 100
+    'Picture Color'             : 'ki 01 00', # 0 - 100
+    'Picture Contrast 2'        : 'kk 01 00', # Not sure why two Contrast Adjustments
+    'Picture Color Temperature' : 'xu 01 00', # 0 - 100
+    'OSD On'                    : 'kl 01 00', # On Screen Display On
+    'OSD Off'                   : 'kl 01 01', # On Screen Display Off
+    'Remote Control Unlock'     : 'km 01 01',
+    'Remote Control Lock'       : 'km 01 00',
+    'Audio Treble'              : 'kr 01 00', # 0 - 100
+    'Audio Bass'                : 'ks 01 00', # 0 - 100
+    'Audio Balance'             : 'kt 01 00'  # 0 - 100
+}
